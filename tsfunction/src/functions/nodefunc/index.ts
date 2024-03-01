67df53c8b0cc15a88c4d4f37ce31b60c75d23e52
nate.delduca@cantire.com <nate.delduca@cantire.com>
2024-03-01 11:59:59 -0500

    adding ts function app for local performance testing

--- tsfunction/src/functions/nodefunc/index.ts
+++ tsfunction/src/functions/nodefunc/index.ts
@@ -1,25 +1,29 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
+import { app, HttpRequest, HttpResponse, HttpResponseInit, InvocationContext } from '@azure/functions';
 const fs = require("fs");
 const csv = require("csv-parser");
- 
+
 export async function tsfunction(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-try {
-    const results = [];
+    try {
+        const results = [];
 
-const startTime = process.hrtime();
+        const startTime = process.hrtime();
 
-fs.createReadStream("sample.csv")
-    .pipe(csv())
-    .on("data", (data) => results.push(data))
-    .on("end", () => {
-        //console.log(results); // Process records as needed
-        const [seconds, nanoseconds] = process.hrtime(startTime);
-        context.info(`Execution time: ${seconds * 1000 + nanoseconds / 1e6}ms`);
-    });
-    
-} catch (error) {
-    return error
-}
+        let res: string;
+        fs.createReadStream("sample.csv")
+            .pipe(csv())
+            .on("data", (data) => results.push(data))
+            .on("end", () => {
+                const [seconds, nanoseconds] = process.hrtime(startTime);
+                res = `Execution time: ${seconds * 1000 + nanoseconds / 1e6}ms`
+                console.log(res);
+            });
+
+        return { status: 200, body: res };
+        
+
+    } catch (error) {
+        return error
+    }
 }
 app.http('tsfunction', {
     methods: ['GET'],
